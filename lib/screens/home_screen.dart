5cd9f96e3bc51f74e62dfd4ff29559465e322b7a
alessandragaleno <alessandragaleno421@gmail.com>
2024-09-27 20:50:34 -0300

    home auterado, com seach adicionado e com cashback

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,9 +7,9 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 67, 132, 235), //0xff => #
+        backgroundColor: Color.fromARGB(255, 67, 132, 235),
         titleTextStyle: TextStyle(
-          color: Colors.white, // Cor do texto de AppBar
+          color: Colors.white,
           fontSize: 20,
         ),
         leading: Builder(
@@ -22,10 +22,8 @@ class HomeScreen extends StatelessWidget {
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
-            // Foto de perfil do usuário no lado direito
             child: GestureDetector(
               onTap: () {
-                // Ação ao clicar na foto de perfil
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -38,24 +36,25 @@ class HomeScreen extends StatelessWidget {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
-                            //exibir a imagrm arredondada
                             ClipOval(
-                              child: Image.asset('lib/assets/images/alexya.png',
-                              width: 200, // largura da imagem
-                              height: 200, 
-                              fit: BoxFit.cover, //faz a imagem cobrir o circulo
-                              // altura da imagem
+                              child: Image.asset(
+                                'lib/assets/images/alexya.png',
+                                width: 200,
+                                height: 200,
+                                fit: BoxFit.cover,
                               ),
-                              ),
-                              SizedBox(height: 10),
-                               TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop(); //fecha a foto
-                                },
-                          child: Text("Fechar"),
+                            ),
+                            SizedBox(height: 10),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("Fechar"),
+                            ),
+                          ],
                         ),
-                      ],),
-                    ), );
+                      ),
+                    );
                   },
                 );
               },
@@ -86,44 +85,94 @@ class HomeScreen extends StatelessWidget {
               leading: Icon(Icons.home),
               title: Text('Página Inicial'),
               onTap: () {
-                // Ação ao clicar
-                Navigator.pop(context); // Fecha o drawer
+                Navigator.pop(context);
               },
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Configurações'),
               onTap: () {
-                // Ação ao clicar
-                Navigator.pop(context); //Fecha o drawer
+                Navigator.pop(context);
               },
             ),
             ListTile(
               leading: Icon(Icons.exit_to_app),
               title: Text('Sair'),
               onTap: () {
-                // Ação ao clicar
-                Navigator.pop(context); // Fecha o drawer
+                Navigator.pop(context);
               },
             ),
           ],
         ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Image.asset(
-              'lib/assets/images/café.png', // caminho para imagem local
-              width: 150,
-              height: 150,
+            // Caixa de pesquisa logo abaixo da AppBar
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                onChanged: (value) {
+                  print('Texto digitado: $value');
+                },
+                decoration: InputDecoration(
+                  hintText: 'Pesquisar...',
+                  prefixIcon: Icon(Icons.search), // Ícone à esquerda
+                  filled: true, // Preencher o fundo
+                  fillColor: Colors.white, // Cor de fundo
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30), // Bordas arredondadas
+                    borderSide: BorderSide.none, // Remove a borda
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30), // Bordas arredondadas
+                    borderSide: BorderSide.none, // Remove a borda
+                  ),
+                ),
+              ),
+            ),
+            // Novo botão adicionado abaixo da caixa de pesquisa
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Aqui você pode adicionar a lógica para o botão
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Botão abaixo da barra de pesquisa clicado!')),
+                  );
+                },
+                child: Text('Cashback 20%'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 45.0),
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/form');
-              },
-              child: Text('Ir para o formulário'),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'lib/assets/images/café.png',
+                    width: 150,
+                    height: 150,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/form');
+                    },
+                    child: Text('Ir para o formulário'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
